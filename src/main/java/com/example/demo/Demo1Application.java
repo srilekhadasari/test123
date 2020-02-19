c3e190ce70a13cf55c138761306970067a1b07bd
srilekha <srilekha851@gmail.com>
2020-02-19 02:03:34 -0800

    commit

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Demo1Application {
+public class Demo1Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	return builder.sources(Demo1Application.class);
+}
 
 }
